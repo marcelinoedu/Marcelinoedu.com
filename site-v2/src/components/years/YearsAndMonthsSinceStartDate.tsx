6e362252b5ee0df5cef51bdcf37d7a53fdca892b
Eduardo Marcelino <eduardoofficial12@gmail.com>
2023-12-26 18:17:10 -0300

    ajustes

---
+++ site-v2/src/components/years/YearsAndMonthsSinceStartDate.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+interface YandMProps{
+    startDate: string
+}
+
+function YearsAndMonthsSinceStartDate({ startDate }:YandMProps) {
+  const [years, setYears] = useState(0);
+  const [months, setMonths] = useState(0);
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const start = new Date(startDate);
+
+    let diffYears = currentDate.getFullYear() - start.getFullYear();
+    let diffMonths = currentDate.getMonth() - start.getMonth();
+
+    if (diffMonths < 0) {
+      diffYears--;
+      diffMonths += 12;
+    }
+
+    setYears(diffYears);
+    setMonths(diffMonths);
+  }, [startDate]);
+
+  return (
+    <div>
+      <p style={{fontSize:"12px"}}>{years} {years === 1 ? 'year' : 'years'} and {months} {months === 1 ? 'month' : 'months'}</p>
+    </div>
+  );
+}
+
+export default YearsAndMonthsSinceStartDate;
